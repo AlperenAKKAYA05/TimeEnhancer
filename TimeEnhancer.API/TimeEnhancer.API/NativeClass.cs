173c5835292266bd765ef78b10c1c0df96c075e7
alperenAKKAYA <alperenakkaya@yandex.com>
2018-08-23 00:55:09 +0300

    Time Enhancer | v1.0.0.0 | 10.04.2018
    Time Enhancer | v1.0.0.0 | 10.04.2018

---
+++ TimeEnhancer.API/TimeEnhancer.API/NativeClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TimeEnhancer
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    internal struct NativeClass
+    {
+        public IntPtr VirtualTable;
+    }
+}
